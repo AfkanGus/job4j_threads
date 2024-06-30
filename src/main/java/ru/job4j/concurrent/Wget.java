d06ebf6de2276736dca645306208d9fce6ef9b4a
AfkanGus <af.kan@bk.ru>
2024-06-30 22:02:04 +0500

    4. Режим ожидания. [#231217].

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -1,18 +1,17 @@
 package ru.job4j.concurrent;
 
 /**
- * 4. Режим ожидания. [#231217].
+ * п
  * Программа выводит через 1 секунду
  * на консоль информацию о загрузке с обновлением строки.
  */
 public class Wget {
     public static void main(String[] args) {
         Thread thread = new Thread(
-                () ->
-                {
+                () -> {
                     try {
                         for (int i = 0; i < 100; i++) {
-                            /*Символ \r указывает, что каретку каждый раз нужно вернуть в начало строки.*/
+                            /* \r - каретка будет возвращаться в начало строки.*/
                             System.out.print("\rLoading : " + i + "%");
                             Thread.sleep(1000);
                         }
