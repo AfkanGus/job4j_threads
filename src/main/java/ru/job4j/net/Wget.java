1aff9e6f22ca8de5aa5ffddad5bba6776a6e4014
AfkanGus <af.kan@bk.ru>
2024-07-13 10:55:56 +0500

    8. Скачивание файла с ограничением. [#144271 #504769]

--- src/main/java/ru/job4j/net/Wget.java
+++ src/main/java/ru/job4j/net/Wget.java
@@ -12,30 +12,39 @@ import java.nio.file.Files;
 public class Wget implements Runnable {
     private final String url;
     private final int speed;
+    private final String fileName;
 
     public Wget(String url, int speed) {
         this.url = url;
         this.speed = speed;
+        this.fileName = url.substring(url.lastIndexOf('/') + 1);
     }
 
     @Override
     public void run() {
         var startAt = System.currentTimeMillis();
-        var file = new File("DownLoadingFile.xml");
+        var file = new File(fileName);
         try (var input = new URL(url).openStream();
              var output = new FileOutputStream(file)) {
             System.out.println("Open connection: "
                     + (System.currentTimeMillis() - startAt) + " ms");
+
             byte[] buffer = new byte[1024];
             int bytesRead;
+            long startTime = System.currentTimeMillis();
+            int totalBytesRead = 0;
+
             while ((bytesRead = input.read(buffer, 0, buffer.length)) != -1) {
-                long startTime = System.currentTimeMillis();
                 output.write(buffer, 0, bytesRead);
-                long timeTaken = System.currentTimeMillis() - startTime;
-                long expectTime = (long) ((1000.0 / speed) * bytesRead);
-                long sleepTime = expectTime - (timeTaken / 1000000);
-                if (sleepTime > 0) {
-                    Thread.sleep(sleepTime);
+                /*суммируем общее число скачанных байт*/
+                totalBytesRead += bytesRead;
+                if (totalBytesRead >= speed) {
+                    long timeTaken = System.currentTimeMillis() - startTime;
+                    if (timeTaken < 1000) {
+                        Thread.sleep(1000 - timeTaken);
+                    }
+                    totalBytesRead = 0;
+                    startTime = System.currentTimeMillis();
                 }
             }
             System.out.println("Download complete. File size: "
