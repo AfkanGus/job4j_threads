a40c4f9492cc660ece1206d8e82b50c0cd232efe
AfkanGus <af.kan@bk.ru>
2024-07-28 18:18:06 +0500

    10. Многопоточность в Stream API [#504930].

--- src/main/java/ru/job4j/concurrent/ParallelStreamExample.java
+++ src/main/java/ru/job4j/concurrent/ParallelStreamExample.java
@@ -3,6 +3,7 @@ package ru.job4j.concurrent;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 /**
@@ -10,7 +11,7 @@ import java.util.stream.Stream;
  * распараллеливания Stream - работа,методы.
  */
 public class ParallelStreamExample {
-    /*все операции выполняются последовательно в главной нити Main*/
+    /*все операции выполняются  sequention потоке в главной нити Main*/
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
         /*reduce - редукция, перемножает эл.сп.*/
@@ -19,10 +20,27 @@ public class ParallelStreamExample {
         /*если список не пусть будет напечатал значение Optional*/
         System.out.println(multiplication);
 
+        /*Операции выполняются в parallel потоке*/
         Stream<Integer> stream = list.parallelStream();
         System.out.println(stream.isParallel());
         Optional<Integer> multiplication1 = list.stream()
                 .reduce((left, right) -> left * right);
         System.out.println(multiplication1.get());
+
+        /*применям метод к паралельному потоку, вернется сам поток*/
+        IntStream parallel = IntStream.range(1, 100).parallel();
+        System.out.println(parallel.isParallel());
+        /*вернется новый паралельный поток*/
+        IntStream sequential = parallel.sequential();
+        System.out.println(sequential.isParallel());
+        /*в многопоточной среде нет гарантии сохранения порядка следования элементов
+         * это неустранимыый побочный эффект метода peek()*/
+        list.stream().parallel().peek(System.out::println).toList();
+        /*forEach. При параллельной обработке потока он так же будет выдавать искаженный порядок элементов*/
+        list.stream().parallel().forEach(System.out::println);
+        /*сохранения порядка следования элементов можно воспользоваться методов forEachOrdered():*/
+        list.stream().parallel().forEachOrdered(System.out::println);
+
     }
+
 }
