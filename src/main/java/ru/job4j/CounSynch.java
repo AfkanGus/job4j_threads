ab8a11b7c4cc9a4db3bd2e2e778cc7e9f48ae9cc
AfkanGus <af.kan@bk.ru>
2024-08-08 22:35:13 +0500

    1. Синхронизация общих ресурсов. [#1096].

--- src/main/java/ru/job4j/CounSynch.java
+++ src/main/java/ru/job4j/CounSynch.java
@@ -7,24 +7,27 @@ package ru.job4j;
  * то одна из нитей блокируется, до тех пор пока первая нить не закончит
  * работу с эти методом
  */
-public class CounSynch { /*не явный манитор, когда манитором будет сам объект класса - CountSynch*/
+public class CounSynch {
+    /*не явный манитор, когда манитором будет сам объект класса - CountSynch*/
     private int valueS;
 
     /* Монитор будет сам класс CounSynch */
     public static synchronized void lockOfClass() {
         synchronized (CounSynch.class) {
-
+            return;
         }
     }
 
     /* Монитор - это объект CounSynch */
     public void lockOfInstance() {
         synchronized (this) {
+            return;
         }
     }
 
     /*Пример не явного указания манитора
-     *При заходе нити в syncronized метод класса, другая нить зайдет в метод после того как первая нить его освободит*/
+     *При заходе нити в syncronized метод класса, другая нить
+     * зайдет в метод после того как первая нить его освободит*/
     public synchronized void increment() {
         valueS++; /*коод внитру метода - критическая секция - обл.памят. где может работать 1 нить*/
         /* Монитор - это объект CounSynch */
