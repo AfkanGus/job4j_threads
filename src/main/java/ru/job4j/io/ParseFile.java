fbb61f7c6a67fe2c672da437df9c5587d1c71ac0
AfkanGus <af.kan@bk.ru>
2024-09-16 23:51:45 +0500

    1. Visibility. Общий ресурс вне критической секции [#1102].

--- src/main/java/ru/job4j/io/ParseFile.java
+++ src/main/java/ru/job4j/io/ParseFile.java
@@ -1,35 +1,40 @@
 package ru.job4j.io;
 
 import java.io.*;
+import java.util.HashSet;
 import java.util.function.Predicate;
 
 /**
  * 1. Visibility. Общий ресурс вне критической секции [#1102].
  */
-public final class ParseFile {
-    private final File file;
+public class ParseFile {
+    private File file;
 
     public ParseFile(File file) {
         this.file = file;
     }
 
     public synchronized String getContent(Predicate<Character> filter) throws IOException {
-        StringBuilder out = new StringBuilder();
-        try (BufferedReader reader = new BufferedReader(new
-                InputStreamReader(new FileInputStream(file)))) {
-            int data;
-            while ((data = reader.read()) != 1) {
-                char character = (char) data;
-                if (filter.test(character)) {
-                    out.append(character);
+        StringBuilder output = new StringBuilder();
+        int data;
+        try (BufferedInputStream in = new BufferedInputStream(new FileInputStream(file))) {
+            while ((data = in.read()) > 0) {
+                if (filter.test((char) data)) {
+                    output.append((char) data);
                 }
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        return out.toString();
+        return output.toString();
     }
 
     public synchronized String getContentWithoutUnicode() throws IOException {
-        return getContent((ch -> ch < 0x80));
+        return getContent(ch -> ch < 0x80);
+    }
+
+    public synchronized String getContent() throws IOException {
+        return getContent(ch -> true);
     }
 }
 
