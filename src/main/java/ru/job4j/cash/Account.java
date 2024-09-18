b30c162b3100076e600336fe3e54a665f784c1ec
AfkanGus <af.kan@bk.ru>
2024-09-19 02:00:16 +0500

    3. Денежные переводы AccountStorage [#1104].

---
+++ src/main/java/ru/job4j/cash/Account.java
@@ -0,0 +1,7 @@
+package ru.job4j.cash;
+
+/**
+ * 3. Денежные переводы AccountStorage [#1104].
+ */
+public record Account(int id, int amount) {
+}
---
+++ src/main/java/ru/job4j/cash/AccountStorage.java
@@ -0,0 +1,55 @@
+package ru.job4j.cash;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.HashMap;
+import java.util.Optional;
+
+/**
+ * 3. Денежные переводы AccountStorage [#1104].
+ */
+@ThreadSafe
+public class AccountStorage {
+    @GuardedBy("this")
+    private final HashMap<Integer, Account> accountHashMap = new HashMap<>();
+
+    public synchronized boolean add(Account account) {
+        if (accountHashMap.containsKey(account.id())) {
+            return false;
+        }
+        accountHashMap.put(account.id(), account);
+        return true;
+    }
+
+    public synchronized boolean update(Account account) {
+        if (!accountHashMap.containsKey(account.id())) {
+            return false;
+        }
+        accountHashMap.put(account.id(), account);
+        return true;
+    }
+
+    public synchronized void delete(int id) {
+        accountHashMap.remove(id);
+
+    }
+
+    public synchronized Optional<Account> getById(int id) {
+        return Optional.ofNullable(accountHashMap.get(id));
+    }
+
+    public synchronized boolean transfer(int fromId, int toId, int amount) {
+        Account fromIdAccount = accountHashMap.get(fromId);
+        Account toIdAccount = accountHashMap.get(toId);
+        if (fromIdAccount != null && toIdAccount != null && fromIdAccount.amount() >= amount) {
+            Account updatedFromAccount = new Account(fromIdAccount.id(),
+                    fromIdAccount.amount() - amount);
+            Account updatedToAccount = new Account(toIdAccount.id(), toIdAccount.amount() + amount);
+            accountHashMap.put(fromId, updatedFromAccount);
+            accountHashMap.put(toId, updatedToAccount);
+            return true;
+        }
+        return false;
+    }
+}
