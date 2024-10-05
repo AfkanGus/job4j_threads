a209279223a807c30416b0f58fa0c4bae3baaefd
afkan_guseynov <af.kan@bk.ru>
2024-10-05 18:32:33 +0500

    4. ThreadSafe динамический список  [#1105]

--- src/main/java/ru/job4j/synchronizers/SingleLockList.java
+++ src/main/java/ru/job4j/synchronizers/SingleLockList.java
@@ -1,4 +1,4 @@
-package ru.job4j.synch;
+package ru.job4j.synchronizers;
 
 import net.jcip.annotations.GuardedBy;
 import net.jcip.annotations.ThreadSafe;
--- src/test/java/ru/job4j/ru/job4j/synchronizers/SingleLockListTest.java
+++ src/test/java/ru/job4j/ru/job4j/synchronizers/SingleLockListTest.java
@@ -1,4 +1,4 @@
-package ru.job4j.synch;
+package ru.job4j.synchronizers;
 
 import org.junit.jupiter.api.Test;
 
