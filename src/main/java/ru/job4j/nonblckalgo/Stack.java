7c5b7acd4b5e7fbc315ad2ab1f595f76c0b2d397
AfkanGus <af.kan@bk.ru>
2024-11-25 16:44:50 +0500

    . CAS - операции [#6859]

---
+++ src/main/java/ru/job4j/nonblckalgo/Stack.java
@@ -0,0 +1,46 @@
+package ru.job4j.nonblckalgo;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+import net.jcip.annotations.ThreadSafe;
+
+/**
+ * 0. CAS - операции [#6859]
+ * Процессоры на уровне ядра поддерживают операцию
+ * compare-and-swap. Эта операция атомарная.
+ *
+ * @param <T>
+ */
+public class Stack<T> {
+    private Node<T> head;
+
+    public void push(T value) {
+        Node<T> temp = new Node<>(value);
+        if (head == null) {
+            head = temp;
+            return;
+        }
+        temp.next = head;
+        head = temp;
+    }
+
+    public T poll() {
+        Node<T> temp = head;
+        if (temp == null) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        head = temp.next;
+        temp.next = null;
+        return temp.value;
+    }
+
+    private static final class Node<T> {
+        private final T value;
+
+        private Node<T> next;
+
+        public Node(final T value) {
+            this.value = value;
+        }
+    }
+}
